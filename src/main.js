92801d5b7c02d2f17005c9efadea8af453833b5d
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-20 23:10:51 -0300

    Altera logo de acordo com a bandeira

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./css/index.css"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
     const colors = {
@@ -12,6 +13,7 @@ function setCardType(type) {
 
     ccBgColor01.setAttribute("fill", colors[type][0])
     ccBgColor02.setAttribute("fill", colors[type][1])
+    ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCardType("visa")
+setCardType("mastercard")
